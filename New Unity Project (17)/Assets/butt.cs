6a538ca143c6eefd04c104d290a685200e9492cb
Student <student@mirea.ru>
2019-11-22 11:09:40 +0300

    button script

---
+++ New Unity Project (17)/Assets/butt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class butt : MonoBehaviour
+{
+    public Slider Slider; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PrintBUtton()
+    {
+        print("Button");
+
+    }
+
+    public void Scrollchange()
+    {
+
+        print(Slider.value);
+
+    }    
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

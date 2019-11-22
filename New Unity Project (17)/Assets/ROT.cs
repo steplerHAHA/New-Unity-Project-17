32e52eb1d561fa77f370e48a45e8433af5280414
Student <student@mirea.ru>
2019-11-22 09:52:59 +0300

    Cloned working project
    Cloned working project

---
+++ New Unity Project (17)/Assets/ROT.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ROT : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Touch touch in Input.touches)
+            if (touch.phase == TouchPhase.Moved)
+            {
+                transform.Rotate(new Vector3(0, -touch.deltaPosition.x, 0));
+            }
+
+    }
+}
---
+++ New Unity Project (17)/Assets/scLE.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scLE : MonoBehaviour
+{
+    float LastDist;
+    GameObject Moving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+    void ScaleObject()
+    {
+        float CurrDist = (Input.touches[0].position - Input.touches[1].position).magnitude;
+        if (LastDist != 0)
+        {
+           Moving.transform.localScale += Vector3.one * (LastDist - CurrDist) * Time.deltaTime * 0.5f;
+                 
+        }
+        LastDist = CurrDist;
+        foreach(Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Ended)
+            {
+                LastDist = 0;
+            }
+
+        }
+    }
+
+
+
+
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touches.Length == 2)
+        {
+            ScaleObject();
+
+        }
+    }
+}

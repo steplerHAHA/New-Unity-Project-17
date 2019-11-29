eb3be04dff4aece14ef0bc5f057edc37439ae7bf
Student <student@mirea.ru>
2019-11-29 10:39:16 +0300

    sounds and animation

---
+++ New Unity Project (17)/Assets/audioffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioffect : MonoBehaviour
+{
+    [SerializeField] AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void Playsound()
+    {
+        audio.Play();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+     if (Input.GetButtonDown("Jump"))
+        {
+            audio.Play();
+        }
+    }
+}
